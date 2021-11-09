295e3c896f5f7d4d98cc5d2ac906ca3e117dc070
ounols <ounols@naver.com>
2021-11-09 23:48:38 +0900

    🍙

---
+++ main.py
@@ -0,0 +1,49 @@
+import time
+import naver_form
+
+if __name__ == '__main__':
+    print("Naver form")
+
+    file = open("info.txt", "r", encoding='utf8')
+    strings = file.readlines()
+    file.close()
+
+    url = strings[0]
+    driver = naver_form.getDriver(url)
+
+    # naver_form.insertForm(driver, 1, strings[1])
+    # time.sleep(2)
+    # naver_form.insertForm(driver, 3, strings[2])
+    # time.sleep(2)
+    # naver_form.insertForm(driver, 4, strings[3])
+    # time.sleep(2)
+    # naver_form.insertForm(driver, 2, "아니오")
+    # time.sleep(2)
+    # naver_form.insertForm(driver, 5, "아니오")
+    # time.sleep(2)
+    # naver_form.insertForm(driver, 7, "아니오")
+    # time.sleep(2)
+    # naver_form.insertForm(driver, 8, "아니오")
+    # time.sleep(2)
+    # naver_form.insertForm(driver, 11, "확인하였음")
+    # time.sleep(2)
+    # # naver_form.submit(driver)
+    # time.sleep(1)
+    # naver_form.close(driver)
+
+    naver_form.insertForm(driver, 14, strings[1])
+    time.sleep(2)
+    naver_form.insertForm(driver, 17, strings[2])
+    time.sleep(2)
+    naver_form.insertForm(driver, 18, strings[3])
+    time.sleep(2)
+    naver_form.insertForm(driver, 15, "아니오")
+    time.sleep(2)
+    naver_form.submit(driver)
+    time.sleep(2)
+    if naver_form.isSubmit(driver):
+        print("success")
+
+    naver_form.close(driver)
+
+
---
+++ naver_form.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pyperclip
+
+
+def getDriver(url):
+    # 드라이버 로딩
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    options.add_argument("lang=ko_KR")
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    options.add_argument("--no-sandbox")
+
+    # chrome driver
+    driver = webdriver.Chrome('chromedriver', chrome_options=options)
+    driver.implicitly_wait(3)
+    driver.get(url)
+    driver.maximize_window()
+
+    time.sleep(2)
+
+    return driver
+
+
+def insertForm(driver, index, value):
+    form_raw = driver.find_element_by_id('formItem_' + str(index))
+    type = form_raw.get_attribute("class")
+    driver.execute_script("arguments[0].scrollIntoView();", form_raw)
+
+    if type == 'formItemPh text':
+        answer = form_raw.find_element_by_id('answer')
+        answer.send_keys(value)
+    elif type == 'formItemPh singleChoice vertical':
+        form_raw.find_element_by_css_selector("div[value='" + str(value) + "']").find_element_by_class_name(
+            "radio").click()
+    elif type == 'formItemPh paragraph':
+        answer = form_raw.find_element_by_id('answer')
+        answer.send_keys(value)
+
+    print("type = " + type)
+
+
+def submit(driver):
+    submit_raw = driver.find_element_by_css_selector("button[menu='submitBtn']")
+    driver.execute_script("arguments[0].scrollIntoView();", submit_raw)
+    submit_raw.click()
+
+def isSubmit(driver):
+    return driver.find_element_by_class_name('finishMessage') == "소중한 의견 감사합니다."
+
+
+def close(driver):
+    driver.close()
